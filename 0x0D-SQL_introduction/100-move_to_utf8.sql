ae91ae2d40f9ec6a3763239cb5268600d423a0c2
Owusu Ansah Joy <owusujoyansah@gmail.com>
2024-06-16 09:41:27 -0700

    Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,8 +1,4 @@
 -- Converting the database to UTF8
 ALTER DTABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Converts the table to UTF8
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Converts the field to UTF8
 ALTER TABLE first_table MOODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
